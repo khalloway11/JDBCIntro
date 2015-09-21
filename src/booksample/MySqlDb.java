b0e20216183c4e851239f9a4d72667b9519f2923
Keiji <Keiji@ProgTop>
2015-09-21 16:23:57 -0500

    init

---
+++ src/booksample/MySqlDb.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package booksample;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author Keiji
+ */
+public class MySqlDb {
+    //class variables
+    private static final String SELECT_ALL = "SELECT * FROM ";
+    private Connection conn;
+    
+    public void openConnection(String driverClass, String url, String userName, String password) throws Exception {
+        Class.forName (driverClass);
+	conn = DriverManager.getConnection(url, userName, password);
+    }
+    
+    public void closeConnection() throws SQLException {
+        conn.close();
+    }
+    
+    public List<Map<String, Object>> findAllRecords(String tableName) throws SQLException{
+        List<Map<String, Object>> records = new ArrayList<>();
+        String sql = SELECT_ALL + tableName;
+
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        ResultSetMetaData meta = rs.getMetaData();
+        int colCount = meta.getColumnCount();
+        
+        while(rs.next()){
+            Map<String, Object> record = new HashMap<>();
+            for(int i = 1; i <= colCount; i++){
+                record.put(meta.getColumnName(i), rs.getObject(i));
+            }
+            records.add(record);
+        }
+        
+        return records;
+    }
+    
+    public void deleteById(String tableName, String PKName, Object target) throws SQLException{
+        //delete from (table) where id = id
+    }
+    
+    public void deleteByField(String tableName, String fieldName, Object target) throws SQLException{
+        
+    }
+    
+    public void updateById(String tableName, String PKName, Object newValue) throws SQLException{
+        
+    }
+    
+    public void updateByField(String tableName, Object fieldName, Object newValue) throws SQLException{
+        
+    }
+    
+    public void createRecord(String tableName, Object[] newRecordInfo) throws SQLException{
+        
+    }
+    
+    public static void main(String[] args) throws Exception{
+        MySqlDb db = new MySqlDb();
+        db.openConnection("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/book", "root", "admin");
+        List<Map<String, Object>> records = db.findAllRecords("author");
+        for(Map record : records){
+            System.out.println(record);
+        }
+        db.closeConnection();
+    }
+}
